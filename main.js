5aeab64dfb12f46a33d429d82381b13cf84fdef6
Admin <reanajb@gmail.com>
2023-11-22 11:24:45 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/************************************************** */
+/****************YOUR CODE HERE************* */
+
+
+
+
+/******************************OUR CODE BELOW******************************** */
+
+if (typeof add === 'undefined') {
+    console.log(add)
+    add = undefined;
+}
+if (typeof subtract === 'undefined') {
+    subtract = undefined;
+}
+if (typeof multiply === 'undefined') {
+    multiply = undefined;
+}
+if (typeof divide === 'undefined') {
+    divide = undefined;
+}
+if (typeof square === 'undefined') {
+    square = undefined;
+}
+if (typeof squareRoot === 'undefined') {
+    squareRoot = undefined;
+}
+if (typeof concatenate === 'undefined') {
+    concatenate = undefined;
+}
+
+module.exports = { add, subtract, multiply, divide, square, squareRoot, concatenate }
